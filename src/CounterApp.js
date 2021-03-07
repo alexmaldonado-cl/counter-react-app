cfb14981c82cc5ac141a94d8f6903f0431feffb9
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-07 19:32:09 -0300

    Add useState in handleAdd, handleSubtract and handleReset functions

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types'
 
-const CounterApp = ({value}) => {
+const CounterApp = ({value = 10}) => {
 
-    const handleAdd = (event) => {
-        console.log(event);
-    }
+    const [counter, setCounter] = useState(value);
+    const handleAdd             = () => setCounter(counter + 1);
+    const handleSubtract        = () => setCounter(counter - 1);
+    const handleReset           = () => setCounter(value);
+    
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
 
             <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubtract }>-1</button>
         </>
     );
 }

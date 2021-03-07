d06f653cfa7d4a3f95c56431c3320a95a1a8b920
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-06 22:49:55 -0300

    Testing props functionality

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const PrimeraApp = () => {
+const PrimeraApp = ({saludo = 'Hola Mundo'}) => {
 
     return (
         <>
-            <h1>Hola mundo</h1>
+            <h1>{saludo}</h1>
+            {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
             <p>Mi primera app</p>
         </>
     );

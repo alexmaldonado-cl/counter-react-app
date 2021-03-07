8f99718a54557a92b8d1f0ccf031aa27df1e1a6f
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-07 19:01:26 -0300

    Add new component 'CounterApp' with an onClick event

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PrimeraApp = ({saludo = 'Hola Mundo'}) => {
+const PrimeraApp = ({saludo, subtitulo}) => {
+
+    // if(!saludo){
+    //     throw new Error('El saludo es requerido');
+    // }
 
     return (
         <>
             <h1>{saludo}</h1>
             {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
-            <p>Mi primera app</p>
+            <p>{subtitulo}</p>
         </>
     );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'My first app'
 }
 
 export default PrimeraApp;
